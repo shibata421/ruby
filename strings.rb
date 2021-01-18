a5d93dc0432a36632bd9cbff33b1fa9dc203990f
Fernando Shibata <shibata.421@gmail.com>
2021-01-18 14:41:04 -0300

    Working with numeric values and string

---
+++ strings.rb
@@ -0,0 +1,17 @@
+greeting = 'Hello'
+puts greeting
+
+target = 'World'
+puts target
+
+puts greeting + ' ' + target
+
+greeting << ' world'
+puts greeting
+
+puts 5 * 3
+puts "5" * 3
+puts greeting.reverse
+puts greeting.upcase
+puts greeting.reverse.capitalize
+puts greeting.length
