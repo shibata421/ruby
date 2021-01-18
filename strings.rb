156ca9324be0d1f5ca07b28a710a680568bd8479
Fernando Shibata <shibata.421@gmail.com>
2021-01-18 15:07:59 -0300

    Testing double and single quotes for Strings

--- strings.rb
+++ strings.rb
@@ -11,7 +11,21 @@ puts greeting
 
 puts 5 * 3
 puts "5" * 3
+
 puts greeting.reverse
 puts greeting.upcase
 puts greeting.reverse.capitalize
 puts greeting.length
+
+puts 'Let\'s escape'
+puts "I said, \"Let's escape!\""
+
+puts "\ta\tb\nc\n\td"
+puts '\ta\tb\nc\n\td'
+
+msg = 'I love you'
+puts "I just called to say: #{msg}"
+puts "1 + 1 = #{ 1 + 1 }"
+
+puts 'I just called to say: #{msg.upcase}'
+puts "I just called to say: #{msg.upcase}"
