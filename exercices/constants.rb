b262bbe20b1fae34487c2277f8d64773bf763417
Fernando Shibata <shibata.421@gmail.com>
2021-01-18 20:36:27 -0300

    Working with constants

---
+++ exercices/constants.rb
@@ -0,0 +1,11 @@
+# To use constants, just name it all caps
+p MAX_SCORE = 100
+
+# If it was already initialized, ruby will only complain that you 
+# changed its value, but will run the
+p MAX_SCORE = 50
+
+# Actually, only the first letter needs to be capitalized, but all 
+# caps is the convention
+p Temp = 100
+p Temp = 1
