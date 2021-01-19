4ae7bb36e6859b61b5100277830052a1a1813f36
Fernando Shibata <shibata.421@gmail.com>
2021-01-18 21:03:18 -0300

    Challenge: convert number into roman numerals

---
+++ challenges/1.rb
@@ -0,0 +1,25 @@
+#Use Ruby to convert a number into a Roman numeral (1 to 10 only)
+
+number_roman = { 
+    1 => 'I',
+    2 => 'II',
+    3 => 'III',
+    4 => 'IV',
+    5 => 'V',
+    6 => 'VI',
+    7 => 'VII',
+    8 => 'VIII',
+    9 => 'IX',
+    10 => 'X',
+}
+
+puts number_roman[1]
+puts number_roman[2]
+puts number_roman[3]
+puts number_roman[4]
+puts number_roman[5]
+puts number_roman[6]
+puts number_roman[7]
+puts number_roman[8]
+puts number_roman[9]
+puts number_roman[10]
